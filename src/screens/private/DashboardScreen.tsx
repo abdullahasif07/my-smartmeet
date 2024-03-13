2023d504f5b6e8290987c1a4ee882270d71a6eec
Snakerelite <mustafahayat202@gmail.com>
2024-03-13 17:58:42 +0500

    Integrated calender to dashboard

--- src/screens/private/DashboardScreen.tsx
+++ src/screens/private/DashboardScreen.tsx
@@ -28,6 +28,10 @@ const App: React.FC = () => {
           </div>
         </div>
       </div>
+      {/* Calendar component */}
+      <div className="calendar-container">
+        <Calendar />
+      </div>
     </div>
   );
 };
