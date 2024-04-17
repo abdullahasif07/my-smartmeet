3428e107a5e8e45022623b71ed98609271698d7f
Snakerelite <mustafahayat202@gmail.com>
2024-04-17 23:22:20 +0500

    Small Changes

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,6 @@
-import {
-  combineReducers,
-  configureStore,
-} from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
+// Import necessary functions and libraries
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 import {
   persistStore,
   persistReducer,
@@ -12,41 +10,32 @@ import {
   PURGE,
   REGISTER,
   REHYDRATE,
-} from 'redux-persist';
-import authReducer from '@/features/auth/authSlice';
-import { userApiSlice } from '@/services/user/userApiSlice';
-import { messageApiSlice } from '@/services/message/messageApiSlice';
-
+} from "redux-persist";
+import authReducer from "@/features/auth/authSlice";
+import { userApiSlice } from "@/services/user/userApiSlice";
+import { messageApiSlice } from "@/services/message/messageApiSlice";
+// Configuration for redux-persist
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  whitelist: ['auth'],
+  whitelist: ["auth"],
 };
-
+// Combine all reducers into a single root reducer
 const rootReducer = combineReducers({
   auth: authReducer,
   [userApiSlice.reducerPath]: userApiSlice.reducer,
   [messageApiSlice.reducerPath]: messageApiSlice.reducer,
 });
+// Create a persisted reducer using the persistConfig and the rootReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const persistedReducer = persistReducer(
-  persistConfig,
-  rootReducer
-);
-
+// Configure and create the redux store
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [
-          FLUSH,
-          REHYDRATE,
-          PAUSE,
-          PERSIST,
-          PURGE,
-          REGISTER,
-        ],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     })
       .concat(userApiSlice.middleware)
@@ -55,5 +44,7 @@ export const store = configureStore({
 
 export const persistor = persistStore(store);
 
+// Define RootState type for use in useSelector hook
 export type RootState = ReturnType<typeof store.getState>;
+// Define AppDispatch type for use in useDispatch hook
 export type AppDispatch = typeof store.dispatch;
